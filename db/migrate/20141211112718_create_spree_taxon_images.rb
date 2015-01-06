5e1672a4e6e20d4fa7665f4ee82e33f773267264
Sanad Liaquat <sanadhussain@gmail.com>
2015-01-06 17:41:29 +0500

    Bug fix

--- db/migrate/20141211112718_create_spree_taxon_images.rb
+++ db/migrate/20141211112718_create_spree_taxon_images.rb
@@ -2,6 +2,7 @@ class CreateSpreeTaxonImages < ActiveRecord::Migration
   def change
     create_table :spree_taxon_images do |t|
       t.references :taxon
+      t.text       :alt
       t.timestamps
     end
   end
