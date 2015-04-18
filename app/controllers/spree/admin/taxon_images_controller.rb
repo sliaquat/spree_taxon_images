3e5ab140af10cdac070ed628ac3c69e9af8dcb34
Sanad Liaquat <sanadhussain@gmail.com>
2015-04-18 14:22:59 +0500

    Added ability to edit

--- app/controllers/spree/admin/taxon_images_controller.rb
+++ app/controllers/spree/admin/taxon_images_controller.rb
@@ -7,10 +7,27 @@ class Spree::Admin::TaxonImagesController < Spree::Admin::ResourceController
   end
 
   def new
-
     @taxon = Spree::Taxon.find(params[:taxon_id])
     @taxon_image = Spree::TaxonImage.new()
+  end
 
+
+  def edit
+    @taxon = Spree::Taxon.find(params[:taxon_id])
+    @taxon_image = @taxon.taxon_images.find_by(id: params[:id])
+  end
+
+  def update
+
+    @taxon_image = Spree::TaxonImage.find(taxon_image[:id])
+
+    if(@taxon_image.update(taxon_image))
+      flash[:success] = "Successfully updated taxon image"
+    else
+      flash[:error] = "Error updating taxon image"
+    end
+
+    redirect_to admin_taxon_taxon_images_url(params["taxon_image"]["taxon_id"])
   end
 
   def create
@@ -33,7 +50,7 @@ class Spree::Admin::TaxonImagesController < Spree::Admin::ResourceController
   private
 
   def taxon_image
-    params.require(:taxon_image).permit(:attachment, :alt, :taxon_id)
+    params.require(:taxon_image).permit(:attachment, :alt, :taxon_id, :id)
   end
 
 end
